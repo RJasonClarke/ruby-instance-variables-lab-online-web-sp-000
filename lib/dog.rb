0adf2bbef390714eadd2885acad94ef30d813d91
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-27 20:26:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,9 @@
 class Dog
-  def this_dogs_name
-    lassie = Dog.new
-    lassie.name=("Lassie")
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+
+  def name
+    this_dogs_name
   end
 end
